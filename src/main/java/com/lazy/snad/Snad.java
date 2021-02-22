4ee0a599b42e54c939557c8a97dbd8739c644afe
lazynessmind <lazynessmind@gmail.com>
2021-02-22 21:44:45 +0000

    Removed proxies

--- src/main/java/com/lazy/snad/Snad.java
+++ src/main/java/com/lazy/snad/Snad.java
@@ -1,26 +1,19 @@
 package com.lazy.snad;
 
-import com.lazy.snad.proxy.ClientProxy;
-import com.lazy.snad.proxy.CommonProxy;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.loading.FMLPaths;
 
-@Mod(Snad.MODID)
+@Mod(Snad.MOD_ID)
 public class Snad {
 
-    public static final String MODID = "snad";
-
-    public static CommonProxy proxy;
+    public static final String MOD_ID = "snad";
 
     public Snad() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Configs.COMMON_CONFIG);
-
-        proxy = DistExecutor.safeRunForDist(() -> ClientProxy::new, () -> CommonProxy::new);
-        proxy.init();
-
         Configs.load(Configs.COMMON_CONFIG, FMLPaths.CONFIGDIR.get().resolve("snad-common.toml"));
+
+        Setup.init();
     }
 }
