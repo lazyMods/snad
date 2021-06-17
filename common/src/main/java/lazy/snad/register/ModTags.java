c31b8c93436d8932baba207dff66471b2168218d
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-06-17 19:10:40 +0100

    Add valid plant tag.

--- common/src/main/java/lazy/snad/register/ModTags.java
+++ common/src/main/java/lazy/snad/register/ModTags.java
@@ -9,8 +9,10 @@ import net.minecraft.world.level.block.Block;
 public class ModTags {
 
     public static Tag.Named<Block> SNAD;
+    public static Tag.Named<Block> VALID_PLANT;
 
     public static void init(){
         SNAD = TagHooks.getBlockOptional(new ResourceLocation(Snad.MOD_ID, "snad"));
+        VALID_PLANT = TagHooks.getBlockOptional(new ResourceLocation(Snad.MOD_ID, "valid_plant"));
     }
 }
