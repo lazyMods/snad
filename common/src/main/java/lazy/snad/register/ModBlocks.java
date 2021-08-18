4da5a574b0139f5402e67287f84ed82513d0834f
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-08-19 00:00:18 +0100

    Add a Soul Sand version of Snad the Soul Snad

--- common/src/main/java/lazy/snad/register/ModBlocks.java
+++ common/src/main/java/lazy/snad/register/ModBlocks.java
@@ -2,6 +2,7 @@ package lazy.snad.register;
 
 import lazy.snad.Snad;
 import lazy.snad.block.SnadBlock;
+import lazy.snad.block.SoulSnadBlock;
 import me.shedaniel.architectury.registry.DeferredRegister;
 import me.shedaniel.architectury.registry.RegistrySupplier;
 import net.minecraft.core.Registry;
@@ -14,6 +15,7 @@ public class ModBlocks {
 
     public static final RegistrySupplier<Block> SNAD = BLOCKS.register("snad", () -> new SnadBlock(-2370656, MaterialColor.SAND));
     public static final RegistrySupplier<Block> RED_SNAD = BLOCKS.register("red_snad", () -> new SnadBlock(-5679071, MaterialColor.SAND));
+    public static final RegistrySupplier<Block> SOUL_SNAD = BLOCKS.register("soul_snad", SoulSnadBlock::new);
 
     public static void init() {
         BLOCKS.register();
