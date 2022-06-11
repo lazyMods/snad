4c6270b66f92284b3b827d67ab405e7772e1eb3e
lazynessmind <lazynessmind@gmail.com>
2022-06-11 02:56:20 +0100

    Fix Tag changes

--- src/main/java/lazy/snad/block/SnadBlock.java
+++ src/main/java/lazy/snad/block/SnadBlock.java
@@ -12,6 +12,7 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FallingBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.common.IPlantable;
--- src/main/java/lazy/snad/register/ModTags.java
+++ src/main/java/lazy/snad/register/ModTags.java
@@ -4,15 +4,16 @@ import lazy.snad.Snad;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.Tag;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
 public class ModTags {
 
-    public static Tag.Named<Block> SNAD;
-    public static Tag.Named<Block> VALID_PLANT;
+    public static TagKey<Block> SNAD;
+    public static TagKey<Block> VALID_PLANT;
 
     public static void init() {
-        SNAD = BlockTags.createOptional(new ResourceLocation(Snad.MOD_ID, "snad"));
-        VALID_PLANT = BlockTags.createOptional(new ResourceLocation(Snad.MOD_ID, "valid_plant"));
+        SNAD = BlockTags.create(new ResourceLocation(Snad.MOD_ID, "snad"));
+        VALID_PLANT = BlockTags.create(new ResourceLocation(Snad.MOD_ID, "valid_plant"));
     }
 }
