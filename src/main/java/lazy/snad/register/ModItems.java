0f3bb823396bb889be3ee59510a6e15d1d7ba32b
marcosantos98 <marcomsantos98@gmail.com>
2023-02-04 17:16:42 +0000

    Fix creative tabs

--- src/main/java/lazy/snad/register/ModItems.java
+++ src/main/java/lazy/snad/register/ModItems.java
@@ -2,7 +2,6 @@ package lazy.snad.register;
 
 import lazy.snad.Snad;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,12 +12,9 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Snad.MOD_ID);
-
-    //TODO: This is here for when I figure out how to create data gens
-
-    public static final RegistryObject<Item> SNAD = ITEMS.register("snad", () -> new BlockItem(ModBlocks.SNAD.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
-    public static final RegistryObject<Item> RED_SNAD = ITEMS.register("red_snad", () -> new BlockItem(ModBlocks.RED_SNAD.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
-    public static final RegistryObject<Item> SOUL_SNAD = ITEMS.register("soul_snad", () -> new BlockItem(ModBlocks.SOUL_SNAD.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
+    public static final RegistryObject<Item> SNAD = ITEMS.register("snad", () -> new BlockItem(ModBlocks.SNAD.get(), new Item.Properties()));
+    public static final RegistryObject<Item> RED_SNAD = ITEMS.register("red_snad", () -> new BlockItem(ModBlocks.RED_SNAD.get(), new Item.Properties()));
+    public static final RegistryObject<Item> SOUL_SNAD = ITEMS.register("soul_snad", () -> new BlockItem(ModBlocks.SOUL_SNAD.get(), new Item.Properties()));
 
     public static void init() {
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
